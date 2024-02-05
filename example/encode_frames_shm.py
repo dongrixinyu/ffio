38754d15c098ead263177a25a19461e3617a4ece
koisi <kor.coeo@gmail.com>
2024-02-05 19:41:51 +0800

    [~][=](shm) update shm test script.

--- example/encode_frames_shm.py
+++ example/encode_frames_shm.py
@@ -23,36 +23,50 @@ def test():
                                     shm_name=your_shm.name, shm_size=shm_size, shm_offset=some_data_bytes
                                     )
 
-  time_last  = time.time()
-  time_begin = time_last
-  time_total = 0
-  idx        = 10
-  rgb_index  = idx % 30
-  # If your shm contains more than one frame,
-  # you can specify an offset to place the RGB bytes in the desired location.
-  # Just do it like this:
-  rgbs = np.ndarray((30, 1080, 1920, 3), dtype=np.uint8, buffer=your_shm.buf, offset=some_data_bytes)
-  while i_pipe.decode_one_frame_to_shm( offset=(rgb_index * 1080 * 1920 * 3) ):
+  if i_pipe.stream_state and o_pipe.stream_state:
+    time_total = 0
+    idx        = 10
+    in_index   = idx % 30
+    # If your shm contains more than one frame,
+    # you can specify an offset to place the RGB bytes in the desired location.
+    # Just do it like this:
+    rgbs = np.ndarray((30, 1080, 1920, 3), dtype=np.uint8, buffer=your_shm.buf, offset=some_data_bytes)
+    while i_pipe.decode_one_frame_to_shm( offset=(in_index * 1080 * 1920 * 3) ) and idx < 500:
+      time_last = time.time()
       # Access the rgb data:
-      frame = rgbs[rgb_index]
       # Maybe you want to make push slower than pull, than you can do like this:
-      output_idx = (idx - 10) % 30
-      _draw(rgbs[output_idx], idx)  # The cv2.rectangle() operates in-place.
-      o_pipe.encode_one_frame_from_shm( offset=output_idx*1080*1920*3 )
+      out_idx    = (idx - 10) % 30
+      out_offset = out_idx*1080*1920*3
+      frame      = rgbs[out_idx]
+      # Same as above:
+      # frame = np.ndarray((1080, 1920, 3), dtype=np.uint8, buffer=your_shm.buf, offset=some_data_bytes+out_offset)
+      _draw(frame, idx)        # The cv2.rectangle() operates in-place.
+      o_pipe.encode_one_frame_from_shm( offset=out_offset )
 
       dt          = time.time() - time_last
-      time_last  += dt
-      time_total  = time_last - time_begin
+      time_total += dt
       idx        += 1
-      rgb_index   = idx % 30
-      avg         = time_total * 1000 / idx
+      in_index    = idx % 30
+      avg         = time_total * 1000 / (idx-10)
       fps         = 1000 / avg
       print(f"{idx}: dt:{dt * 1000:.2f}ms, avg:{avg:.2f}ms, {fps}fps, "
             f"total: {time_total:.3f}s, shape:{frame.shape}")
-
-  i_pipe.release_memory()
-  o_pipe.release_memory()
+  # Attention !!
+  # Force quitting this script will result in a memory leak.
+  # Ensure the following process is executed.
+    i_pipe.release_memory()
+    o_pipe.release_memory()
+  your_shm.close()
+  your_shm.unlink()
   print('successfully release memory of input stream context.')
+
+  # If you quit without cleanup the memory,
+  # you can find the shm in /dev/shm/, name like: psm_b699387a
+  # Than you can clean it manually:
+  # from multiprocessing import shared_memory
+  # _shm = shared_memory.SharedMemory(name="psm_b699387a")
+  # _shm.close()
+  # _shm.unlink()
 
 def _draw(frame: np.ndarray, seq: int):
   diff    = seq % 100
