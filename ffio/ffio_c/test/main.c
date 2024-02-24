83ffb210aa932baf36e3a0f13c9ed6dc106ce80a
koisi <kor.coeo@gmail.com>
2024-02-25 07:30:16 +0800

    [~][=] new ffio: c test case: main.c

--- ffio/ffio_c/test/main.c
+++ ffio/ffio_c/test/main.c
@@ -20,35 +20,49 @@ static void saveRGB2File(const FFIO* ffio){
 }
 
 int main(int argc, char *argv[]){
-  if(argc >= 2){ printf("Running with: %s %s\n", argv[0], argv[1]); }
-  else{ printf("Failed to run with wrong arguments.\n"); exit(1); }
+  if( argc == 1 ){ printf("Please run with args: i_url [o_url] [gpu|cpu].\n"); exit(1); }
+  printf("Running with: %s %s %s.\n", argv[0], argv[1], argc == 3 ? argv[2] : "" );
 
-  if(strncmp(argv[1], "rtmp://", 7) != 0){
-    printf("Please provide a correct rtmp url.\n");
-    exit(1);
-  }
+  char *i_url = argv[1];
+  char *o_url = NULL;
   bool hw_enabled = false;
-  if(argc>=3 && strncmp(argv[2], "gpu", 3)==0 ){ hw_enabled = true; }
+  if(argc==3){
+    if( strncmp(argv[2], "gpu", 3) == 0 ){ hw_enabled = true; }
+    else{ o_url = argv[2]; }
+  }else if(argc>3){
+    o_url = argv[2];
+    if( strncmp(argv[3], "gpu", 3) == 0 ){ hw_enabled = true; }
+  }
 
-  char* rtmp = argv[1];
-  FFIO* ffio = newFFIO();
+  FFIO* i_ffio = newFFIO();
+  FFIO* o_ffio = newFFIO();
 
-  CodecParams params;
-  initFFIO(ffio, FFIO_MODE_DECODE, rtmp,
+  CodecParams params = {
+      0, 0, 0, 0, 0, 0,
+      "", "", "", "", ""
+  };
+  initFFIO(i_ffio, FFIO_MODE_DECODE, i_url,
            hw_enabled, NULL,
            false, NULL, 0, 0, &params);
+  if(o_url!=NULL){
+    initFFIO(o_ffio, FFIO_MODE_ENCODE, o_url,
+             hw_enabled, NULL,
+             false, NULL, 0, 0, &params);
+  }
 
   int ret;
-  for(int i=0; i<10; ++i){
-    ret = decodeOneFrame(ffio);
-    printf("decodeOneFrame returned %d, image seq: %d.\n", ret, ffio->frameSeq);
-    if(ret == 0){
-      saveRGB2File(ffio);
+  for(int i=0; i<200; ++i){
+    ret = decodeOneFrame(i_ffio);
+    printf("[%d] decodeOneFrame returned %d.\n", i_ffio->frameSeq, ret);
+    if( ret==0 && i<10){ saveRGB2File(i_ffio); }
+    if( ret==0 && o_url!=NULL){
+      ret = encodeOneFrame(o_ffio, i_ffio->rawFrame);
+      printf("[%d] encodeOneFrame returned %d.\n", o_ffio->frameSeq, ret);
     }
   }
 
-  finalizeFFIO(ffio);
-  finalizeFFIO(ffio);
+  finalizeFFIO(i_ffio);
+  finalizeFFIO(o_ffio);
 
   return 0;
 }
